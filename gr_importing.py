8b7a38f7e158f17ad865c01f9544b82f62629dfd
Owen <owenlu93@gmail.com>
2023-07-07 17:39:45 -0700

    feat: Added gradio application for input interface

---
+++ gr_importing.py
@@ -0,0 +1,24 @@
+import gradio as gr
+from aligned_vision_qb import create_invoice
+
+# Inputs
+client = gr.inputs.Textbox(default = 'Aligned Vision')
+date = gr.inputs.Textbox(label='Date MM-DD-YYYY')
+invoice_num = gr.inputs.Textbox()
+clockify_excel = gr.inputs.File(label='Excel export of clockify hours')
+inputs = [client,date,invoice_num,clockify_excel]
+
+# Outputs
+outputs = gr.outputs.File()
+
+# Functions
+def process_csv(client,date,invoice_num,excel_file):
+    output_fp = create_invoice(excel_file.name,client,date,invoice_num)
+
+    return output_fp
+
+# Objects
+iface = gr.Interface(process_csv,inputs=inputs, outputs = outputs)
+
+# Run the interface
+iface.launch(inbrowser=True)
